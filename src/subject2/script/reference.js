68353d61499f0c8116e4274e32d8ef5f7684222b
yubin7885 <you@37887106+yubin7885@users.noreply.github.com>
2023-10-03 06:02:18 +0900

    일단,,

--- src/subject2/script/reference.js
+++ src/subject2/script/reference.js
@@ -1,31 +1,9 @@
 const reference = [
   {
-    author: "lena polishko",
-    title: "",
+    author: "김도경",
+    title: "201920200",
     year: "",
     sourceTitle: "",
-    link: "https://unsplash.com/@ilon_a",
-  },
-
-  {
-    author: "ash thorp",
-    title: "M O D U L A R - YouTube",
-    year: "2021",
-    sourceTitle: "",
-    link: "https://vimeo.com/486511696",
-  },
-  {
-    author: "ash thorp",
-    title: "FITC Tokyo 2015 Titles",
-    year: "2015",
-    sourceTitle: "",
-    link: "https://vimeo.com/118919656",
-  },
-  {
-    author: "GitHub, Inc.",
-    title: "What is GitHub?",
-    year: "2022",
-    sourceTitle: "",
-    link: "https://www.youtube.com/watch?v=pBy1zgt0XPc",
+    link: "",
   },
 ];
